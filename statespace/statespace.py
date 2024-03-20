253184b573291f289862d79f8e9052b2376cc397
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-03-19 23:57:23 -0700

    add directional inline/sidestep logic

--- statespace/statespace.py
+++ statespace/statespace.py
@@ -96,6 +96,22 @@ from pprint import pprint
 import debugutils
 import external
 
+absolute_directions = [10, 11, 1]
+
+bounds = {
+    0: (5, 4),
+    1: (1, 5),
+    2: (1, 6),
+    3: (1, 7),
+    4: (1, 8),
+    5: (1, 9),
+    6: (2, 9),
+    7: (3, 9),
+    8: (4, 9),
+    9: (5, 9),
+    10: (5, 4)
+}
+
 
 def genall_groupmove_resultboard(marbles: dict[int, int],
                                  player_color: int)\
@@ -116,36 +132,44 @@ def genall_groupmove_resultboard(marbles: dict[int, int],
         coord = int
         color = int
     """
-    debugutils.print_board(marbles)
+    # debugutils.print_board(marbles)
     # print("marbles:")
     # pprint(marbles)
     # print("player_color:", player_color)
 
-    player_marbles = filter_for_color(marbles, player_color)
+    player_marbles = dict(filter(lambda marble: marble[1] == player_color,
+                                 marbles.items()))
     # print("player_marbles:")
     # pprint(player_marbles)
 
-    all_groupmoves = genall_groupmoves()
-    print(all_groupmoves)
-    pprint(all_groupmoves)
+    all_groupmoves = genall_groupmoves(marbles, player_marbles)
+    # print("all_groupmoves:")
+    # pprint(all_groupmoves)
 
 
-def genall_groupmoves(marbles: dict[int, int]) \
+def genall_groupmoves(board: dict[int, int],
+                      player_marbles: dict[int, int]) \
         -> list[tuple[tuple[tuple[int, int], ...], int]]:
-    """Generates all groupmoves given a dict of marbles.
+    """Generates all groupmoves given a dict of marbles and the current board.
 
     return format explained:
         list[groupmove]
     """
     inlinegroupmoves, sidestepgroupdirs = \
-        genall_inlinegroupmoves_sidestepgroupdirs()
-    # sidestep_moves = gen_sidemoves(sidestep_groupdirs)
+        genall_inlinegroupmoves_sidestepgroupdirs(board, player_marbles)
+    # print("inline groupmoves:")
+    # pprint(inlinegroupmoves)
+    # print("sidestep groupdirs:")
+    # pprint(sidestepgroupdirs)
+
+    # sidestep_moves = gen_sidestepgroupmoves(sidestep_groupdirs)
     # return inline_moves + sidestep_moves
     return None
 
 
-def genall_inlinegroupmoves_sidestepgroupdirs(marbles: dict[int, int]) \
-        -> tuple[list[tuple[tuple[int, int], ...], int],
+def genall_inlinegroupmoves_sidestepgroupdirs(board: dict[int, int],
+                                              player_marbles: dict[int, int]) \
+        -> tuple[list[tuple[tuple[tuple[int, int], ...], int]],
                  list[tuple[tuple[int, int], ...], int]]:
     """Generates all inline groupmoves and sidestep groupdirs.
 
@@ -153,17 +177,81 @@ def genall_inlinegroupmoves_sidestepgroupdirs(marbles: dict[int, int]) \
     lists in one go, but it does make the code a little more complicated
 
     return format explained:
-        list[inline_groupmoves, sidestep_groupdirs]
+        output = (list[inline_groupmove], list[sidestep_groupdirs])
 
         inline_groupmoves = list[groupmove]
         sidestep_groupdirs = list[groupdirs]
     """
+    inline_groupmoves = []
+    sidestep_groupdirs = []
+
+    for marble in player_marbles.items():
+        for direction in absolute_directions:
+            new_inline_groupmoves, new_sidestep_groupdirs = \
+                derive_direction_inlinegroupmove_sidestepgroupdirs(board,
+                                                                   marble,
+                                                                   direction)
+
+
     return None, None
 
-def filter_for_color(marbles: dict[int, int],
-                     color: int) -> dict[int, int]:
-    """filters the board out for the colors we are looking for."""
-    return dict(filter(lambda marble: marble[1] == color, marbles.items()))
+def derive_direction_inlinegroupmove_sidestepgroupdirs(board: dict[int, int],
+                                             marble: tuple[int, int],
+                                             direction) \
+    -> tuple[tuple[tuple[tuple[int, int], ...], int],
+             list[tuple[tuple[int, int], ...], int]]:
+    """Get marble's inline groupmove, sidestep groupdirs, for single direction.
+
+    return format explained:
+        output = (inlinegroupmove, list[sidestepgroupdir])
+    """
+    sidestep_groupdirs = []
+    cur_grouping = [marble]
+    next_coord = marble[0] + direction
+    num_players = 1
+    num_enemies = 0
+
+    # print("cur_grouping", cur_grouping)
+    # print("next_coord", next_coord)
+
+    while True:
+        # if next == AVAILABLE: break
+        try:
+            # throws KeyError if no marble at that coordinate
+            next_marble = (next_coord, board[next_coord])
+        except KeyError:
+            print("terminate")
+            break
+
+        if is_out_of_bounds(next_coord) \
+                or num_players == num_enemies \
+                or num_players > 3:
+            print("terminate")
+            break
+
+        cur_grouping.append(next_marble)
+        print(cur_grouping)
+
+        if next_marble[1] == marble[1]:
+            sidestep_groupdirs.append((cur_grouping, direction))
+            num_players += 1
+        elif next_marble[1] == 1 - marble[1]:
+            num_enemies += 1
+
+        next_coord += direction
+
+    if is_out_of_bounds(next_coord) and cur_grouping[-1][1] == marble[1]:
+        return (None, sidestep_groupdirs)
+
+
+    return None, None
+
+
+def is_out_of_bounds(marble_coord: int) -> bool:
+    print(marble_coord)
+    row = marble_coord // 10
+    col = marble_coord % 10
+    return col < bounds[row][0] or col > bounds[row][1]
 
 
 if __name__ == "__main__":
@@ -172,7 +260,7 @@ if __name__ == "__main__":
 
     board_marbles, player_color = external.in_to_marbles(in_base+"Test1.input")
     result = genall_groupmove_resultboard(board_marbles, player_color)
-    print("groupmoves_resultboards:", result)
+    # print("groupmoves_resultboards:", result)
 
     # board_marbles, player_color = external.in_to_marbles(in_base+"Test2.input")
     # result = genall_groupmove_resultboard(board_marbles, player_color)
