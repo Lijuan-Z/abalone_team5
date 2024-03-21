dbd5b80c30e817c2a8f174dc1d28fb9d30003f39
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-03-21 00:05:34 -0700

    add new debug lines

--- statespace/debug.py
+++ statespace/debug.py
@@ -35,10 +35,9 @@ def print_output_file_line(output_file_line):
 
 
 if __name__ == '__main__':
-    lines = """c5b,d5b,e4b,e5b,e6b,e7b,f5b,f6b,f8b,g6b,h6b,c3w,c4w,d3w,d4w,d6w,d7w,f4w,f7w,g5w,g7w,g8w,g9w,h8w,h9w
-c5b,d5b,e4b,e5b,e6b,f5b,f6b,f7b,f8b,g5b,h6b,c3w,c4w,d3w,d4w,d6w,e7w,f4w,g4w,g6w,g7w,g9w,h7w,h8w,h9w
-c5b,d5b,e5b,e6b,f4b,f5b,f6b,f7b,f8b,g6b,h6b,c3w,d3w,d4w,d6w,e4w,e7w,g4w,g5w,g7w,g8w,g9w,h7w,h8w,h9w
-c5b,d6b,e4b,e5b,e6b,f5b,f6b,f7b,f8b,g6b,h6b,c3w,c4w,d4w,d5w,d7w,e7w,f4w,g5w,g7w,g8w,g9w,h7w,h8w,h9w"""
+    lines = """c5b,d5b,e4b,e5b,e6b,f5b,f6b,f7b,f8b,g6b,h6b,c3w,c4w,d3w,d4w,d6w,e7w,f4w,g5w,g7w,g8w,g9w,h7w,i9w
+c5b,d5b,e4b,e5b,e6b,f5b,f6b,f7b,f8b,g6b,h6b,c3w,c4w,d3w,d4w,d6w,e7w,f4w,g5w,g7w,g8w,g9w,i8w,i9w
+c5b,d5b,e4b,e5b,e6b,f5b,f6b,f7b,f8b,g6b,h6b,c3w,c4w,d3w,d4w,d6w,e7w,f4w,g5w,g7w,g8w,h7w,h8w"""
 
     for line in lines.split('\n'):
         print_output_file_line(line)
