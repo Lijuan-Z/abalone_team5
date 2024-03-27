cd6be2f95d6a51ae609f360841d0f351319ecfca
camwalford <cwalford2012@gmail.com>
2024-03-26 18:22:16 -0700

    Made search_best_move.py runnable on its own and fixed the path to test files used.

--- search_best_move.py
+++ search_best_move.py
@@ -2,10 +2,12 @@
 from statespace import external
 from statespace import statespace
 
-in_base = "tests/in/"
-out_base = "tests/out/"
 
 
-test_num = 2
-board_marbles, player_color = external.in_to_marbles(f"{in_base}Test{test_num}.input")
-statespace.iterative_deepening_alpha_beta_search(board_marbles, 0, 10000, 15)
+if __name__ == '__main__':
+    in_base = "tests/statespace_gen_validation/in/"
+    out_base = "tests/statespace_gen_validation/out/"
+
+    test_num = 2
+    board_marbles, player_color = external.in_to_marbles(f"{in_base}Test{test_num}.input")
+    statespace.iterative_deepening_alpha_beta_search(board_marbles, 0, 10000, 15)
