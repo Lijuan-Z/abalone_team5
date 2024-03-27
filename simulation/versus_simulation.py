257ef6182d5c9d545421f35af1f4ed876138df2c
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-03-27 01:25:13 -0700

    add game simulation

--- simulation/versus_simulation.py
+++ simulation/versus_simulation.py
@@ -2,6 +2,8 @@
 import time
 
 from .base_simulation import Simulation
+from statespace.statespace import iterative_deepening_alpha_beta_search
+from statespace.statespace import apply_move
 
 
 class VersusSimulation(Simulation):
@@ -11,17 +13,19 @@ class VersusSimulation(Simulation):
 
     def start(self):
         """Starts the simulation"""
-        self.board_state[11] = 0
+        self.board_state = {11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 33: 0, 34: 0, 35: 0, 99: 1, 98: 1, 97: 1, 96: 1, 95: 1, 89: 1, 88: 1, 87: 1, 86: 1, 85: 1, 84: 1, 77: 1, 76: 1, 75: 1}
+        player_turn = 0
         self.update_display()
-        time.sleep(1)
 
-        self.board_state[12] = 0
-        self.update_display()
-        time.sleep(1)
 
-        self.board_state[13] = 0
-        self.update_display()
-        time.sleep(1)
+        while True:
+            move = iterative_deepening_alpha_beta_search(self.board_state,
+                                                         player_turn, 20, 30)
+            apply_move(self.board_state, move)
+            self.update_display()
+
+            player_turn = 1 - player_turn
+
 
         while True:
             self.update_display()
