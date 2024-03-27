8ec348172f1857420529c75dae03d3f8caea7485
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-03-26 23:28:11 -0700

    change run_simulation to run a versus simulation while bound to a tkinter display

--- run_simulation_app.py
+++ run_simulation_app.py
@@ -1,7 +1,11 @@
 """Driver program for the simulation app."""
 
 from gui import simulation_app
+from simulation.versus_simulation import VersusSimulation
 
 if __name__ == "__main__":
-    app = simulation_app.SimApp()
+
+    sim_driver = VersusSimulation()
+
+    app = simulation_app.SimApp(sim_driver)
     app.mainloop()
