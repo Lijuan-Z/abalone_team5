2f1529aab9410d768996091b587c07442f97652b
kate <katezraly@gmail.com>
2024-03-28 18:53:49 -0700

    convert switch statement to dictionary call

--- heuristics/kate_heuristic.py
+++ heuristics/kate_heuristic.py
@@ -1,24 +1,18 @@
 """Contains a heuristic that returns random values."""
 
+distance_mapping = {
+    55: 0,
+    65: 1, 66: 1, 54: 1, 56: 1, 44: 1, 45: 1,
+    75: 2, 76: 2, 77: 2, 67: 2, 57: 2, 46: 2, 35: 2, 34: 2, 33: 2, 43: 2, 53: 2, 64: 2,
+    85: 3, 86: 3, 87: 3, 88: 3, 78: 3, 68: 3, 58: 3, 47: 3, 36: 3, 25: 3, 24: 3, 23: 3, 22: 3, 32: 3, 42: 3, 52: 3, 63: 3, 74: 3,
+    95: 4, 96: 4, 97: 4, 98: 4, 99: 4, 89: 4, 79: 4, 69: 4, 59: 4, 48: 4, 37: 4, 26: 4, 15: 4, 14: 4, 13: 4, 12: 4, 11: 4, 21: 4, 31: 4, 41: 4, 51: 4, 62: 4, 73: 4, 84: 4
+}
+
 def middle_control(ply_board, max_player):
     max_count = 0
     min_count = 0
     for pos, col in ply_board.items():
-        # cam's code, thanks cam!
-        match pos:
-            case 55:
-                distance = 0
-            case 65 | 66 | 54 | 56 | 44 | 45:
-                distance = 1
-            case 75 | 76 | 77 | 67 | 57 | 46 | 35 | 34 | 33 | 43 | 53 | 64:
-                distance = 2
-            case 85 | 86 | 87 | 88 | 78 | 68 | 58 | 47 | 36 | 25 | 24 | 23 | 22 | 32 | 42 | 52 | 63 | 74 | 85:
-                distance = 3
-            case 95 | 96 | 97 | 98 | 99 | 89 | 79 | 69 | 59 | 48 | 37 | 26 | 15 | 14 | 13 | 12 | 11 | 21 | 31 | 41 | 51 | 62 | 73 | 84:
-                distance = 4
-            case _:
-                raise ValueError(
-                    f"Value {pos} is not valid. Check that all positions on the board are valid.\n{ply_board}")
+        distance = distance_mapping[pos]
 
         if col == max_player:
             max_count += distance
