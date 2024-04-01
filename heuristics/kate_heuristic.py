18e633369c0dabcd95fbcab2c23caedac96b4016
kate <katezraly@gmail.com>
2024-04-01 16:34:53 -0700

    update kate heuristic

--- heuristics/kate_heuristic.py
+++ heuristics/kate_heuristic.py
@@ -1,11 +1,11 @@
 """Contains a heuristic that returns random values."""
 
 distance_mapping = {
-    55: 0,
+    55: -5,
     65: 1, 66: 1, 54: 1, 56: 1, 44: 1, 45: 1,
     75: 2, 76: 2, 77: 2, 67: 2, 57: 2, 46: 2, 35: 2, 34: 2, 33: 2, 43: 2, 53: 2, 64: 2,
     85: 3, 86: 3, 87: 3, 88: 3, 78: 3, 68: 3, 58: 3, 47: 3, 36: 3, 25: 3, 24: 3, 23: 3, 22: 3, 32: 3, 42: 3, 52: 3, 63: 3, 74: 3,
-    95: 10, 96: 10, 97: 10, 98: 10, 99: 10, 89: 10, 79: 10, 69: 10, 59: 10, 48: 10, 37: 10, 26: 10, 15: 10, 14: 10, 13: 10, 12: 10, 11: 10, 21: 10, 31: 10, 41: 10, 51: 10, 62: 10, 73: 10, 84: 10
+    95: 4, 96: 4, 97: 4, 98: 4, 99: 4, 89: 4, 79: 4, 69: 4, 59: 4, 48: 4, 37: 4, 26: 4, 15: 4, 14: 4, 13: 4, 12: 4, 11: 4, 21: 4, 31: 4, 41: 4, 51: 4, 62: 4, 73: 4, 84: 4
 }
 directions = [1, 10, 11]
 
@@ -34,34 +34,37 @@ def marble_loss(ply_board, max_player):
 
 def density(ply_board, max_player):
     num = 0
-    den = 0
+    den = 1
     danger_count = [0, 0]
 
     for marble in ply_board.items():
         for direction in directions:
             if marble[0] + direction in ply_board.keys():
-                num += 1 - max_player
+                if marble[1] == max_player:
+                 num += 1
                 if distance_mapping[marble[0]] == 10 and marble[0] + direction in ply_board.keys() and ply_board[marble[0] + direction] == 1 - marble[1] and ply_board[marble[0] + direction] == 1 - marble[1]:
                     danger_count[marble[1]] += 1
             if marble[0] - direction in ply_board.keys():
-                num += 1 - max_player
+                if marble[1] == max_player:
+                    num += 1
                 if distance_mapping[marble[0]] == 10 and marble[0] - direction in ply_board.keys() and ply_board[marble[0] - direction] == 1 - marble[1] and ply_board[marble[0] - direction] == 1 - marble[1]:
                     danger_count[marble[1]] += 1
 
-            den += 1 - max_player
+            if marble[1] == max_player:
+                den += 1
 
     # print("num den", (num/den, num/den))
     # print("danger_count", (danger_count[max_player], danger_count[1 - max_player]))
     # print()
-    return (num / den) + (2 * (danger_count[1 - max_player] - danger_count[max_player]))
+    return (num / den) + 1.5 * (danger_count[1 - max_player] - danger_count[max_player])
 
 
-def eval_state(ply_board, total_turns_remaining, max_player, *args, **kwargs):
+def eval_state(ply_board, max_player, *args, **kwargs):
     """Returns a random evaluation result."""
     heuristics = {
-        middle_control: total_turns_remaining * 0.02,
+        middle_control: 1,
         marble_loss: 20,
-        density: 0.6,
+        density: 2,
     }
 
     sum = 0
