a8f56e2baa930d39c076622fe74f090c62e414f9
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-04-04 02:46:38 -0700

    comment out debug code that went straight to the game, skipping config screen

--- refactor_abalone.py
+++ refactor_abalone.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     game = GameApp()
 
     #TODO: REMOVE AFTER DEV: simulates clicking the start button right away
-    config = game.current_page.display_state._get_config()
-    game.start_game(config=config)
+    # config = game.current_page.display_state._get_config()
+    # game.start_game(config=config)
 
     game.mainloop()
 
