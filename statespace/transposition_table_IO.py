2d58396b9fca4e054b732e470172d0bcd08b1557
camwalford <cwalford2012@gmail.com>
2024-04-04 01:23:15 -0700

    Updated json serialization

--- statespace/transposition_table_IO.py
+++ statespace/transposition_table_IO.py
@@ -7,23 +7,30 @@ def save_transposition_table_to_pickle(table, filename):
         pickle.dump(table, file)
 
 
-def load_transposition_table_from_pickle(filename):
-    with open(filename, 'rb') as file:
-        table = pickle.load(file)
-    return table
-
-
 def save_transposition_table_to_json(table, filename):
-    # Convert keys or values if necessary (e.g., converting tuple keys to strings)
-    serializable_table = {str(key): value for key, value in table.items()}
     with open(filename, 'w') as file:
-        json.dump(serializable_table, file)
+        json.dump(table, file)
+
+
+def load_transposition_table_from_pickle(filename):
+    try:
+        with open(filename, 'rb') as file:
+            table = pickle.load(file)
+    except FileNotFoundError:
+        return {}  # Return an empty dict if file not found
+    return table
 
 
 def load_transposition_table_from_json(filename):
-    with open(filename, 'r') as file:
-        serializable_table = json.load(file)
-    # Convert keys or values back if necessary
-    table = {eval(key): value for key, value in serializable_table.items()}
-    return table
-
+    try:
+        with open(filename, 'r') as file:
+            serializable_table = json.load(file)
+    except FileNotFoundError as e:
+        print(e)
+        print(f"File {filename} not found. Returning an empty dict.")
+        return {}  # Return an empty dict if file not found
+    except json.JSONDecodeError as e:
+        print(e)
+        print(f"File {filename} is empty or cannot be decoded. Returning an empty dict.")
+        return {}
+    return serializable_table
