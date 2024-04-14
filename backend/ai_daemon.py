30ddee59c79c9750accc6f7eb60f8e82dd9e09b1
kate <katezraly@gmail.com>
2024-04-13 17:39:10 -0700

    remove across turn node ordering

--- backend/ai_daemon.py
+++ backend/ai_daemon.py
@@ -12,7 +12,6 @@ class AIDaemon(Process):
         super(AIDaemon, self).__init__()
         self.daemon = True
         self.frontend_conn = frontend_conn
-        self._best_path = []
         self._transposition_table_white = load_transposition_table_from_json('transposition_table_white.json')
         self._transposition_table_black = load_transposition_table_from_json('transposition_table_black.json')
         self._first_move_history = load_from_pickle('first_move_history.pkl')
@@ -28,9 +27,8 @@ class AIDaemon(Process):
             strategy = cam_heuristic.eval_state
             if game_state['player'] == 0 and game_state['is_first_move']:
                 while True:
-                    move, self._best_path, self._transposition_table_black, elapsed_time = iterative_deepening_alpha_beta_search(
+                    move, self._transposition_table_black, elapsed_time = iterative_deepening_alpha_beta_search(
                         eval_callback=strategy,
-                        path=self._best_path[1:],
                         transposition_table=self._transposition_table_black,
                         **game_state
                     )
@@ -42,9 +40,8 @@ class AIDaemon(Process):
                         break
 
             elif game_state['player'] == 0:
-                move, self._best_path, self._transposition_table_black, elapsed_time = iterative_deepening_alpha_beta_search(
+                move, self._transposition_table_black, elapsed_time = iterative_deepening_alpha_beta_search(
                     eval_callback=strategy,
-                    path=self._best_path[1:],
                     transposition_table=self._transposition_table_black,
                     **game_state
                 )
@@ -52,9 +49,8 @@ class AIDaemon(Process):
                 # Uncomment line below if you want the t_table to be saved
                 # save_transposition_table_to_json(self._transposition_table_black, 'transposition_table_black.json')
             else:
-                move, self._best_path, self._transposition_table_white, elapsed_time = iterative_deepening_alpha_beta_search(
+                move, self._transposition_table_white, elapsed_time = iterative_deepening_alpha_beta_search(
                     eval_callback=strategy,
-                    path=self._best_path[1:],
                     transposition_table=self._transposition_table_white,
                     **game_state
                 )
